3d1b72cef9e2e499eb586ac773d61c6d894ea054
Nikolay Rozhkov <rozhkov@uchi.ru>
2017-11-11 11:44:52 +0300

    Blocks for level

--- genlevel.rb
+++ genlevel.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby -W0
 
-# 1. Build an empty room.
 # 2. Place goals in the room.
 # 3. Find the state farthest from the goal state.
 
@@ -11,8 +10,30 @@
 # b - box
 # e - empty
 
-# blocks.yaml
-# YAML magic
-# http://yaml-multiline.info/
-blocks = File.read('blocks.txt').split(/\n{2,}/).compact
-p blocks
+class Block < Array
+  # clock-wise
+  def rotate(n = 1)
+    i = n % 4
+    chunk = self
+    i.times {
+      chunk = Block.new chunk.reverse.transpose
+    }
+    chunk
+  end
+
+  def to_s
+    self.map(&:join).join("\n")
+  end
+end
+
+chunks = File.read('blocks.txt').split(/\n{2,}/).compact
+
+blocks = chunks.map do |chunk|
+  Block.new chunk.split('/n').map{ |line| line.split('') }
+end
+
+# puts blocks.map(&:to_s).join("\n\n")
+
+# 1. Build an empty room.
+
+
