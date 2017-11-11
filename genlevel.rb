68418d7249d44e63a1a83cd86c309f378cc0a388
Nikolay Rozhkov <rozhkov@uchi.ru>
2017-11-11 13:24:40 +0300

    Some random level & fixes

--- genlevel.rb
+++ genlevel.rb
@@ -10,15 +10,40 @@
 # b - box
 # e - empty
 
+BLOCK_SIZE = 3
+
 class Block < Array
   # clock-wise
+  def reverse
+    Block.new super
+  end
+
+  def transpose
+    Block.new super
+  end
+
   def rotate(n = 1)
     i = n % 4
-    chunk = self
+    block = self
     i.times {
-      chunk = Block.new chunk.reverse.transpose
+      block = block.reverse.transpose
     }
-    chunk
+    block
+  end
+
+  def flip(n = 1)
+    i = n % 2
+    block = self
+    i.times {
+      block = block.reverse
+    }
+    block
+  end
+
+  # random rotate or flip
+  # central symmetry is the same as 180 degrees rotate is
+  def randomize
+    self.rotate(rand(4)).flip(rand(2))
   end
 
   def to_s
@@ -29,11 +54,49 @@ end
 chunks = File.read('blocks.txt').split(/\n{2,}/).compact
 
 blocks = chunks.map do |chunk|
-  Block.new chunk.split('/n').map{ |line| line.split('') }
+  Block.new chunk.split("\n").map{ |line| line.split('') }
 end
+
+b = Block.new ('a'..'z').to_a.first(25).each_slice(5).to_a
+# puts b.to_s
+# puts '---'
+# puts b.rotate(2).to_s
 
 # puts blocks.map(&:to_s).join("\n\n")
 
 # 1. Build an empty room.
 
+width = rand 3
+height = rand 3
 
+Field = Struct.new :width, :height, :blocks do
+  def initialize(width, height, blocks = [])
+    super
+  end
+
+  def fill
+    field = self.dup
+    height.times do |y|
+      field.blocks[y] ||= []
+      width.times do |x|
+        field.blocks[y][x] = yield(x, y)
+      end
+    end
+  end
+
+  def to_s
+    height.times.map do |y|
+      BLOCK_SIZE.times.map do |l|
+        width.times.map do |x|
+          blocks[y][x][l][1 .. BLOCK_SIZE].join
+        end.join
+      end.join("\n")
+    end.join("\n")
+  end
+end
+
+f = Field.new 3, 3
+f.fill do |x, y|
+  blocks.sample.randomize
+end
+puts f.to_s
